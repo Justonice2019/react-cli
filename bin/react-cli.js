a3f80d18bd5d9536abdc92e0f257bc20d4460a28
俞连强 <1175409441@qq.com>
2023-05-26 17:07:15 +0800

    _

--- bin/react-cli.js
+++ bin/react-cli.js
@@ -1,4 +1,16 @@
 #!/usr/bin/env node
-console.log('hello2')
 const fs = require('fs')
 const path = require('path')
+const chalk = require('chalk')
+const create = require('../lib/create')
+
+const program = require('commander')
+program
+    .version(`@justonice/react-cli ${require('../package').version}`)
+    .usage('<command> [options]')
+program.command('create <app-name>')
+    .description('create a new project')
+    .action((name, options) => {
+      create(name, options)
+    })
+program.parse()
---
+++ lib/template/src/pages/About/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class About extends React.Component{
+  componentDidMount() {
+    console.log('componentDidMount')
+  }
+
+  render() {
+    return <h1>About~~~</h1>;
+  }
+}
+export default About
