95686389205541270ae29d69dfe6fb0ce9d8a3ed
俞连强 <1175409441@qq.com>
2023-06-21 15:19:46 +0800

    _

--- lib/template/src/pages/About/index.jsx
+++ lib/template/src/pages/About/index.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React from 'react';
 
-class About extends React.Component{
+class About extends React.Component {
   componentDidMount() {
-    console.log('componentDidMount')
+    console.log('componentDidMount');
   }
+
+  onClickGetData = () => {
+    // /api/examples/get/string
+    fetch('/api/examples/get/string?name=Tony&age=28', {
+      method: 'GET',
+    }).then(res => {
+      console.log(res);
+    });
+  };
 
   render() {
-    return <h1>About~~~</h1>;
+    return (<div>
+      <h1>About~221~212232~</h1>
+      <button onClick={this.onClickGetData}>获取数据</button>
+    </div>);
   }
 }
-export default About
+export default About;
